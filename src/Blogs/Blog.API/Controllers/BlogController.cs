358a2b47c1bd82514f20106f834c4f4a27dd985b
AbdullohDev07 <gamesupro01@gmail.com>
2024-05-08 23:53:19 +0500

    Add project files.

---
+++ src/Blogs/Blog.API/Controllers/BlogController.cs
@@ -0,0 +1,58 @@
+﻿using Blog.API.Persistance;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BlogController(AppDbContext appDbContext) : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext = appDbContext;
+
+        [HttpGet]
+        public IEnumerable<Models.Blog> Get()
+        {
+            return [.. _appDbContext.Blogs];
+        }
+
+        [HttpGet]
+        public async Task<List<Models.Blog>> GetById(int id)
+        {
+            return [.. _appDbContext.Blogs];
+        }
+
+        [HttpPost]
+        public async Task<Models.Blog> Post([FromBody] Models.Blog product)
+        {
+            var result = await _appDbContext.Blogs.AddAsync(product);
+
+            await _appDbContext.SaveChangesAsync();
+            return result.Entity;
+        }
+
+        [HttpPut]
+        public async Task<Models.Blog> Put(int id, [FromBody] Models.Blog product)
+        {
+            var pr = await _appDbContext.Blogs.FirstOrDefaultAsync(x => x.Id == id) ?? throw new Exception();
+            pr.Name = product.Name;
+            pr.Description = product.Description;
+            var result = _appDbContext.Blogs.Update(pr).Entity;
+            await _appDbContext.SaveChangesAsync();
+
+            return result;
+        }
+
+        [HttpDelete]
+        public async Task<Models.Blog> Delete(int id)
+        {
+            var pr = await _appDbContext.Blogs.FirstOrDefaultAsync(x => x.Id == id) ?? throw new Exception();
+            var result = _appDbContext.Blogs.Remove(pr).Entity;
+
+            await _appDbContext.SaveChangesAsync();
+
+            return result;
+        }
+    }
+}
---
+++ src/Students/Student.UI/Controllers/StudentController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Student.UI.Persistanse;
+
+namespace Student.UI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class StudentController(AppDbContext appDbContext) : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext = appDbContext;
+
+        [HttpGet]
+        public IEnumerable<Models.Student> Get()
+        {
+            return [.. _appDbContext.Students];
+        }
+
+        [HttpGet]
+        public async Task<List<Models.Student>> GetById(int id)
+        {
+            return [.. _appDbContext.Students];
+        }
+
+        [HttpPost]
+        public async Task<Models.Student> Post([FromBody] Models.Student product)
+        {
+            var result = await _appDbContext.Students.AddAsync(product);
+
+            await _appDbContext.SaveChangesAsync();
+            return result.Entity;
+        }
+
+        [HttpPut]
+        public async Task<Models.Student> Put(int id, [FromBody] Models.Student product)
+        {
+            var pr = await _appDbContext.Students.FirstOrDefaultAsync(x => x.Id == id) ?? throw new Exception();
+            pr.Name = product.Name;
+            pr.Description = product.Description;
+            var result = _appDbContext.Students.Update(pr).Entity;
+            await _appDbContext.SaveChangesAsync();
+
+            return result;
+        }
+
+        [HttpDelete]
+        public async Task<Models.Student> Delete(int id)
+        {
+            var pr = await _appDbContext.Students.FirstOrDefaultAsync(x => x.Id == id) ?? throw new Exception();
+            var result = _appDbContext.Students.Remove(pr).Entity;
+
+            await _appDbContext.SaveChangesAsync();
+
+            return result;
+        }
+    }
+}
